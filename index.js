e585515e07dda7f197cafc4469106f8804843029
auzundere <auzundere@yahoo.com>
2019-12-18 00:43:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ const express = require('express')
 const app = express()
 const util = require('util')
 const port = process.env.PORT || 8080;
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
 // const port = 8080;
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.get('*', (req, res) => {
-  console.log(req.ip);
-  console.log(req.originalUrl);
-  console.log(util.inspect(req.body, {showHidden: false, depth: null}));
-  res.send("OK")
-})
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// app.get('*', (req, res) => {
+//   console.log(req.ip);
+//   console.log(req.originalUrl);
+//   console.log(util.inspect(req.body, {showHidden: false, depth: null}));
+//   res.send("OK")
+// })
 
 app.post('*', (req,res) => {
   console.log(req.ip);
