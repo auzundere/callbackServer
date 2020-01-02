716cefd5b3b92e422004bd187e836bb892a5f7b8
auzundere <auzundere@yahoo.com>
2020-01-02 10:15:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,21 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.get('*',(req,res) => {
+  console.log(req.ip);
+  console.log(req.originalUrl);
+  console.log(util.inspect(req.body, {showHidden: false, depth: null}));
+  if(req.originalUrl = "/refresh") {
+    res.json({
+      access_token:"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJtYXR0aGV3LmFsbGVuQGJsdWV3b2xmZ3JvdXAuY29tIiwic3ViIjoiU0ZEQyB0ZWFtIC0gYmx1ZSB3b2xmICAiLCJqdGkiOiJVTklRVUVJRC1tYXR0aGV3LmFsbGVuIiwic2NvcGUiOlsic2VsZiIsImFkbWlucyJdLCJpYXQiOjE1MjIyNzU4NzYsImV4cCI6MTUzODA4NzA3Nn0.lWBLx0rptkGDNHiSuXoPKClwFvPGgG4GBQAOtSldQyo",
+      token_type: "Bearer",
+      expires_in: 300,
+      scope: "offline_access",
+      refresh_token: "test_Refresh_token"
+    })
+  } else {
+    res.json({status:"ok"})
+  }
 });
 // app.get('*', (req, res) => {
 //   console.log(req.ip);
